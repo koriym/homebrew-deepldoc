53893acd46b70ccc7ff7cecc1d3e0ceff979191c
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-05-31 00:58:37 +0900

    Update build and install commands in deepldoc.rb
    The changes involve modifying the build and install commands in deepldoc.rb. The Go build command now includes a "-x" option for detailed output, and the

--- deepldoc.rb
+++ deepldoc.rb
@@ -4,7 +4,7 @@ class Deepldoc < Formula
   url "https://github.com/koriym/deepldoc/archive/refs/tags/0.2.0.tar.gz"
   sha256 "4bc242290731cdec3f2902c00617144f8bafe9813c437555be5c0c509913b8c8"
   def install
-    system "go", "build", "-o", "deepldoc", "./deepldoc"
-    bin.install "deepldoc"
+    system "go", "build", "-x", "-o", "deepldoc/deepldoc", "./deepldoc"
+    bin.install "deepldoc/deepldoc"
   end
 end
