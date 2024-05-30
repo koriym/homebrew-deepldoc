4414c03f2a27e0719ad7e121d2c394acc40eece8
GitHub Action <action@github.com>
2024-05-30 18:24:07 +0000

    Update to 0.2.0

--- deepl.rb
+++ deepl.rb
@@ -1,7 +1,7 @@
 class Deepl < Formula
   desc "Command line tool for DeepL"
   homepage "https://github.com/koriym/deepldoc"
-  url ""
+  url "https://github.com/koriym/deepldoc/releases/download/0.2.0/deepl"
   sha256 ""
   def install
     bin.install "deepl"
