aed82ab00e374084d87874e65be197d0f614221b
GitHub Action <action@github.com>
2024-05-30 18:29:25 +0000

    Update to 0.2.0

--- deepldoc.rb
+++ deepldoc.rb
@@ -2,7 +2,7 @@ class Deepldoc < Formula
   desc "A tool to translate documents in a directory at once."
   homepage "https://github.com/koriym/deepldoc"
   url "https://github.com/koriym/deepldoc/releases/download/0.2.0/deepldoc"
-  sha256 ""
+  sha256 "702cd5a62751dabb349befbfd82f57fb77fe156f5ddf3ff8b46ed3188990ce7d"
   def install
     bin.install "deepldoc"
   end
