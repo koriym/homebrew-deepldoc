d950d21a148decb0f38f8d3fb1bb6b5897f05c24
Akihito Koriyama <akihito.koriyama@gmail.com>
2024-05-31 03:04:20 +0900

    Remove URL and SHA256 from Deepl and Deepldoc formulas
    The URL and SHA256 fields have been removed from both the Deepl and Deepldoc formulas. Additionally, extraneous system commands have been cleaned up from the Deepldoc install method, making the code more readable and efficient.

--- deepldoc.rb
+++ deepldoc.rb
@@ -1,13 +1,9 @@
 class Deepldoc < Formula
   desc "A tool to translate documents in a directory at once."
   homepage "https://github.com/koriym/deepldoc"
-  url "https://github.com/koriym/deepldoc/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "4bc242290731cdec3f2902c00617144f8bafe9813c437555be5c0c509913b8c8"
+  url ""
+  sha256 ""
   def install
-    system "pwd"
-    system "ls", "-la"
-    system "go", "build", "-x", "-o", "deepldoc/deepldoc", "./deepldoc"
-    system "ls", "-la", "deepldoc"
-    bin.install "deepldoc/deepldoc"
+    bin.install "deepldoc"
   end
 end
